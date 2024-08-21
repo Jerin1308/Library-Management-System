f5849af6112476bfde2bd739a0117c7c1fbaf0a6
Jerin1308 <jerina1308@gmail.com>
2024-08-22 02:27:14 +0530

    complete-backEnd

--- src/main/java/lms/App.java
+++ src/main/java/lms/App.java
@@ -2,8 +2,7 @@ package lms;
 
 import lms.lib.LibManager;
 
-public class App 
-{
+public class App {
     public static void main( String[] args ){
         LibManager manager = new LibManager();
         manager.start();
